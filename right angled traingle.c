1e873b0d724573b7ba4c6f187d5e54e49be0670c
janaharajan <59387615+janaharajan@users.noreply.github.com>
2022-01-05 19:29:06 +0530

    Add files via upload
    C PROGRAM TO PRINT INVERTED RIGHT ANGLED TRIANGLE

---
+++ right angled traingle.c
@@ -0,0 +1,35 @@
+/*C PROGRAM TO PRINT INVERTED RIGHT ANGLED TRIANGLE
+******
+*****
+****
+***
+**
+* coded by janaharajan */
+#include<stdio.h>
+int main()
+{
+ int n,i,j;
+ printf("\nEnter the number of rows :");
+ scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+
+{
+    for(j=n;j>i;j--)
+    {
+
+        printf("*");
+
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+return 0;
+}
